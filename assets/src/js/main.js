b4e4400116c83600b8f3d8abc21f790e58f806e1
Vetl-off0098 <vetl-off@mail.ru>
2020-01-24 16:35:39 +0800

    Линейщик

---
+++ assets/src/js/main.js
@@ -0,0 +1,7 @@
+// Импортируем jQuery
+//= ../../../node_modules/jquery/dist/jquery.min.js
+//= ../../../node_modules/jQuery-Mask/dist/jquery.mask.js
+//= ../../../node_modules/owl.carousel2/dist/owl.carousel.js
+// Импортируем другие js-файлы
+//= my.js
+//= wow.min.js
